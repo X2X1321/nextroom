d227294080165b6e0e5e01749adaaca808aea3ba
X2X <vitenikpavel@gmail.com>
2026-08-05 17:16:29 +0300

    feat: add Vercel build script for collectstatic and migrations

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput --clear
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Build complete!"
