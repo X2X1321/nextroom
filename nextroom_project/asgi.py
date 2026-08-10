eec687496405900744c3edae1dde86e59cfc77b5
X2X <vitenikpavel@gmail.com>
2026-08-10 18:17:59 +0300

    fix: Fix Pillow dependency for image upload, add chat upload spinner, perfectly center responsive error templates, fix asgi.py

--- nextroom_project/asgi.py
+++ nextroom_project/asgi.py
@@ -15,8 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nextroom_project.settings')
 
 application = get_asgi_application()
 
-from .bootstrap import bootstrap  # noqa: E402
-try:
-    bootstrap()
-except Exception:
-    pass
